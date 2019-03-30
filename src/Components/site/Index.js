634af5d8057e761db2d453b09de4990179e798af
joseph <bomber.man87@yahoo.com>
2019-03-31 01:48:25 +0430

    add buy modal

--- src/Components/site/Index.js
+++ src/Components/site/Index.js
@@ -3,30 +3,22 @@ import OurTeam from './OurTeam';
 import TopBrand from './TopBrand';
 import LigtBeam from './LightBeam';
 import Footer from './Footer';
+import OrderBox from './OrderBox';
 import SuperMarioGame from './SuperMario/SuperMarioGame';
 
 
 const Index = () => (
     <React.Fragment>
-
         <TopBrand/>
         <SuperMarioGame/>
 
         <div className={'afterGameDescription'}>
-
-            <LigtBeam />
-            <div className={'offerText'}>میتونین کتاب ها رو از طریق سایت سفارش بدین تا دو تا استیکر خوشگل 😍 با
-                        المان‌های بازی سوپر ماریو به همراه کتاب 📖 امضای شده توسط نویسنده دریافت کنید.
-
-            <div className={'doPaymentBtn ripple'}><i className={'fa fa-shopping-basket'}/> ثبت سفارش</div>
-            </div>
-
+            <LigtBeam/>
+            <OrderBox/>
             <OurTeam/>
-
-
         </div>
 
-        <Footer />
+        <Footer/>
     </React.Fragment>
 );
 
