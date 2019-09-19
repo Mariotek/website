b6112cb583926e582d04d8eaeafcd6d5e1c28a4a
jafarRezaei <bomber.man87@yahoo.com>
2019-09-19 19:16:44 +0430

    bug fix

--- src/actions/csrf_token.js
+++ src/actions/csrf_token.js
@@ -1,3 +1,9 @@
-export const example = {
+export const actionTypes = {
     GET_TOKEN: 'GET_TOKEN'
 };
+
+export const getToken = () => ({
+    type  : actionTypes.GET_TOKEN,
+    remote: '/csrfToken',
+    method: 'get'
+});
