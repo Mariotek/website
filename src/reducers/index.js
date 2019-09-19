f6c993abe27b5541b50de106e1abb3c569e05b4b
jafarRezaei <bomber.man87@yahoo.com>
2019-09-19 15:43:16 +0430

    reduce Reducers 😀

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,12 @@
 import { combineReducers } from 'redux';
 
-import csrf_token from './csrf_token';
-import loginAndRegisterReducer from './loginAndRegister';
+// import csrf_token from './csrf_token';
+// import loginAndRegisterReducer from './loginAndRegister';
 
 
 // Start creating one reducer, and then jump into combining them
-export const makeRootReducer = () =>
-    combineReducers({
-        /**
-         * To get csrf token
-         */
-        csrf_token,
-
-        /**
-         * For login and register
-         */
-        loginAndRegister: loginAndRegisterReducer,
-
-    });
+export const makeRootReducer = () => {
+    return {
+        make: null
+    };
+};
